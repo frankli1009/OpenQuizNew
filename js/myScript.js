2104eef5c48718c160b97ab3b3ec208b85085350
frankli1009 <frankli1009@gmail.com>
2017-08-13 16:46:57 +0100

    Almost finished
    most of the functions has been finished. start to process the situation of api service breaking down.

--- js/myScript.js
+++ js/myScript.js
@@ -4,8 +4,12 @@ function itemInfo(itemName, itemValue) {
 }
 
 var quiz = {
+    online: true,
     questions: null,
-    count: 0,
+    count: 10,
+    category: null,
+    type: null,
+    difficulty: null,
     index: -1,
     done: 0,
     right: 0,
@@ -36,11 +40,11 @@ var curQuestion = {
         this.startTime = new Date();
         
         this.cntdwnInt = setInterval(function() {
-            console.log("myInterval, CountDown: "+curQuestion.iCurSeconds);
+            //console.log("myInterval, CountDown: "+curQuestion.iCurSeconds);
     
             // Time calculations for days, hours, minutes and seconds
             var secs = --curQuestion.iCurSeconds;
-            console.log("secs: " + secs);
+            //console.log("secs: " + secs);
 
             // If the count down is finished, write some text 
             if (secs === 0) {
@@ -55,68 +59,42 @@ var curQuestion = {
     }
 };
 
-function myInterval() {
-
-    console.log("myInterval, CountDown: "+curQuestion.iCurSeconds);
-    
-    // Time calculations for days, hours, minutes and seconds
-    var secs = --curQuestion.iCurSeconds;
-    console.log("secs: " + secs);
-
-    // If the count down is finished, write some text 
-    if (secs < 0) {
-        clearInterval(curQuestion.cntdwnInt);
-        $("#questiontime").text("");
-        nextQuestion();
-    } else {
-        // Display the result in the element with id="questiontime"
-        $("#questiontime").text(secs + " s");
-    }
-}
-
 $(function(){
     $.getJSON("https://opentdb.com/api_category.php")
     .done(function(data) {
-        console.log(data.trivia_categories);
-        data.trivia_categories.forEach(function(category, index) {
-            console.log(category.name);
-            $option = $("<option>");
-            $option.text(category.name);
-            $option.attr("value", category.id);
-            $("#quizcategory").append($option);
-        });
-        $("#quizresult").text("Ready, give it a go...");
+        //console.log(data.trivia_categories);
+        loadCategoriesAndSettings(data);
+        quiz.online = true;
     })
     .fail(function() {
-        console.log("fail to get categories");
-        $("#quizresult").text("Can't load quiz categories, please try it later.");
-        
-    })
-    .always(function() {
-        showResult(true);
+        console.error("fail to get categories");
+        quiz.online = false;
+        loadLocalQuizInfo();
+        //connectionFailure();
     });
     
+    $('#myModal').on('shown.bs.modal', function(){ 
+       addModalBody();
+    });
+    
+    $("#savesettings").on("click", function() {
+        if(saveQuizSettings()) return;
+        else return false;
+    });
+    
+    //avoid key "enter", "." and "E" being input into number of questions
     $("#quizcount").on("keydown", function (e) {
-        //console.log(e.keyCode);
-        switch(e.keyCode) {
-            case 13: // enter key, skipped
-                e.preventDefault();
-                return false;
-            case 46: // period (46, 190) and "E" (69) key, need to be skipped
-                e.preventDefault();
-                return false;
-            case 190: // period (46, 190) and "E" (69) key, need to be skipped
-                e.preventDefault();
-                return false;
-            case 69: // period (46, 190) and "E" (69) key, need to be skipped
-                e.preventDefault();
-                return false;
-        }
+        return skipKeys(e);
+    });
+    
+    //avoid key "enter", "." and "E" being input into time limitation per question
+    $("#quiztime").on("keydown", function(e) {
+        return skipKeys(e);
     });
         
-    $(".answerDiv").on("click", function(e) {
+    $(".answerBorder").on("click", function(e) {
         //console.log(e.target.id);
-        var choice = e.target.id.substring(5,6);
+        var choice = e.target.id.substring(6,7);
         //console.log(choice);
         if(parseInt(choice) === curQuestion.iCorrect) quiz.right++;
         clearInterval(curQuestion.cntdwnInt);
@@ -125,72 +103,164 @@ $(function(){
     });
         
     $("#startquiz").on("click", function() {
-        if(!validCount($("#quizcount").val(), 5, 50)) {
-           alert("Please enter a number between 5 and 50 for number of questions.");
-           return;
-        }
-        if(!validCount($("#quiztime").val(), 3, 60)) {
-           alert("Please enter a number between 3 and 60 for limited time per question.");
-           return;
-        }
-        
-        $("#newgamediv").hide();
+        if(!saveQuizSettings()) return;
+
+        $("#startgamediv").hide();
         $(".questionResultDiv").show();
-        
-        var quizCount = parseInt($("#quizcount").val());
-        quiz.count = quizCount;
-        var quizCategory = $("#quizcategory").val();
-        var quizType = $("#quiztype").val();
-        var quizDifficulty = $("#quizdifficulty").val();
-        quiz.countDown = parseInt($("#quiztime").val());
-        console.log("countdown: "+quiz.countDown);
         
         $("#didit").html("0 / 0");
         
-        var url = "https://opentdb.com/api.php?amount="+quiz.count;
-        if(quizCategory) {
-            url += "&category=" + quizCategory;
-        }
-        if(quizType) {
-            url += "&type=" + quizType;
-        }
-        if(quizDifficulty) {
-            url += "&difficulty=" + quizDifficulty;
-        }
-        console.log(url);
-        
-        $.getJSON(url)
-        .done(function(data) {
-            console.log(data);
-            if(data.response_code === 0) {
-                quiz.initQuiz(data);
-                showNextQuestion();
-                showResult(false);
-            } else {
-                console.log("response_code: "+data.response_code);
-                $("#quizresult").text("Can't load quiz question, please try it later.");
-                showResult(true);
+        var url = "";
+        if(quiz.online) {
+           url += "https://opentdb.com/api.php?amount="+quiz.count; 
+            if(quiz.category) {
+                url += "&category=" + quiz.category;
             }
-            
-            
-        })
-        .fail(function() {
-            console.log("fail to get questions");
-            $("#quizresult").text("Can't load quiz question, please try it later.");
-            showResult(true);
-            $("#didit").html("");
-        });
+            if(quiz.type) {
+                url += "&type=" + quiz.type;
+            }
+            if(quiz.difficulty) {
+                url += "&difficulty=" + quiz.difficulty;
+            }
+            //console.log(url);
+            $.getJSON(url)
+            .done(function(data) {
+                //console.log(data);
+                if(data.response_code === 0) {
+                    quiz.initQuiz(data);
+                    showNextQuestion();
+                    showResult(false);
+                } else {
+                    console.error("response_code: "+data.response_code);
+                    connectionFailure();
+                }
+            })
+            .fail(function() {
+                console.error("fail to get questions");
+                connectionFailure();
+                $("#didit").html("");
+            });
+        } else {
+            getLocalQuestions();
+        }
+         
     });
     
-    $content = $(".quizContent")
-    var width = $content.width
-    $("#chartrightwrong").width = width * 4 / 10
-    $("#charttimeused").width = width * 4 / 10
+    $("#changechart").on("click", function(e) {
+        e.preventDefault();
+        toggleChart();
+    });
+    
+    $("#newgame").on("click", function() {
+        if(curQuestion.cntdwnInt != null) {
+            clearInterval(curQuestion.cntdwnInt);
+            curQuestion.cntdwnInt = null;
+        }
+        
+        $("#startquiz").click();
+    });
 });
 
-function showResult(showRes) {
+//avoid key "enter", "." and "E" being input into the text input element
+function skipKeys(e) {
+    //console.log(e.keyCode);
+    switch(e.keyCode) {
+        case 13: // enter key, skipped
+        case 46: // period (46, 190), skipped
+        case 190: // period (46, 190), skipped
+        case 69: // "E" (69) key, skipped
+            e.preventDefault();
+            return false;
+    }
+    return true;
+}
+
+
+function connectionFailure() {
+    $("#quizresult").text("Load Failure!");
+    $("#chartrightwrong").text("Due to connection error, we can't load quiz now, please try it later.");
+    showResult(true, false);
+}
+
+function loadQuizSettings() {
+    try{
+        iQuizCount = parseInt(localStorage.getItem("quizCount"));
+        if(isNaN(iQuizCount)) {
+           return;
+        }
+        quiz.count = iQuizCount;
+        $("#quizcount").val(quiz.count);
+        quiz.category = localStorage.getItem("quizCategory");
+        $("#quizcategory").val(quiz.category);
+        quiz.type = localStorage.getItem("quizType");
+        $("#quiztype").val(quiz.type);
+        quiz.difficulty = localStorage.getItem("quizDifficulty");
+        $("#quizdifficulty").val(quiz.difficulty);
+        quiz.countDown = parseInt(localStorage.getItem("quizCountDown"));
+        $("#quiztime").val(quiz.countDown);
+    }
+    catch(e) {
+        console.warn("Can't find the original quiz settings.")
+    }
+}
+
+function saveQuizSettings() {
+    if(!validCount($("#quizcount").val(), 5, 50)) {
+       alert("Please enter a number between 5 and 50 for number of questions.");
+       return false;
+    }
+    if(!validCount($("#quiztime").val(), 3, 60)) {
+       alert("Please enter a number between 3 and 60 for limited time per question.");
+       return false;
+    }
+
+    quiz.count = parseInt($("#quizcount").val());
+    quiz.category = $("#quizcategory").val();
+    quiz.type = $("#quiztype").val();
+    quiz.difficulty = $("#quizdifficulty").val();
+    quiz.countDown = parseInt($("#quiztime").val());
+    //console.log("countdown: "+quiz.countDown);
+    
+    localStorage.setItem("quizCount", quiz.count);
+    localStorage.setItem("quizCategory", quiz.category);
+    localStorage.setItem("quizType", quiz.type);
+    localStorage.setItem("quizDifficulty", quiz.difficulty);
+    localStorage.setItem("quizCountDown", quiz.countDown);
+    
+    return true;
+}
+
+function addModalBody() {
+    $modalBody = $(".modal-body");
+    if(!($modalBody.hasClass("ownchild"))) {
+        $settings = $("#settings");
+        $modalBody.append($settings);
+        $modalBody.addClass("ownchild");
+        $settings.show();
+    }
+}
+
+function toggleChart() {
+    var $changeChart = $("#changechart");
+    if($changeChart.text().indexOf("Time")>-1) {
+        $changeChart.html("Right/Wrong");
+        $("#chartrightwrong").addClass("hideChart");
+        $("#charttimeused").removeClass("hideChart");
+    } else {
+        $changeChart.html("Time Used");
+        $("#charttimeused").addClass("hideChart");
+        $("#chartrightwrong").removeClass("hideChart");
+    }
+}
+
+function showResult(showRes, showBtn) {
     if(showRes) {
         $("#questiondiv").hide();
+        if(showBtn) {
+            $(".floatBtn").show();
+        } else {
+            $(".floatBtn").hide();
+        }
         $("#resultdiv").show();
     } else {
         $("#resultdiv").hide();
@@ -211,7 +281,7 @@ function showNextQuestion() {
     
     quiz.index++;
     if(quiz.index < quiz.count) {
-        clearRadioState();
+        //clearRadioState();
         var question = quiz.questions.results[quiz.index]; 
         /*
         $("#questionCategory").text(question.category);
@@ -221,7 +291,7 @@ function showNextQuestion() {
         $("#questionIt").html(question.question);
         var iCount = getAnswerCount(question.type);
         var iCorrect = getCorrectIndex(iCount);
-        console.log("correct: "+iCorrect);
+        //console.log("correct: "+iCorrect);
         for(var i=0; i<iCount; i++) {
             if(i === iCorrect) {
                 $("#ques1"+i).html(question.correct_answer);
@@ -239,24 +309,23 @@ function showNextQuestion() {
         }
         
         if(question.type === "multiple") {
-            if($("#answer1").hasClass("twoAvailAnswers"))
-                $("#answer1").removeClass("twoAvailAnswers");
-            if(!$("#answer1").hasClass("fourAvailAnswers"))
-                $("#answer1").addClass("fourAvailAnswers");
-            if($("#answer2").hasClass("twoAvailAnswers"))
-                $("#answer2").removeClass("twoAvailAnswers");
-            if(!$("#answer2").hasClass("fourAvailAnswers"))
-                $("#answer2").addClass("fourAvailAnswers");
+            removeClass($("#answer1"), "twoAvailAnswers");
+            addClass($("#answer1"), "fourAvailAnswers");
+            removeClass($("#answer2"), "twoAvailAnswers");
+            addClass($("#answer2"), "fourAvailAnswers");
+            addClass($("#answer1"), "borderBottomYellow");
+            addClass($("#answer2"), "borderBottomYellow");
+            
             $("#quesrow2").show();
         } else {
-            if($("#answer1").hasClass("fourAvailAnswers"))
-                $("#answer1").removeClass("fourAvailAnswers");
-            if(!$("#answer1").hasClass("twoAvailAnswers"))
-                $("#answer1").addClass("twoAvailAnswers");
-            if($("#answer2").hasClass("fourAvailAnswers"))
-                $("#answer2").removeClass("fourAvailAnswers");
-            if(!$("#answer2").hasClass("twoAvailAnswers"))
-                $("#answer2").addClass("twoAvailAnswers");
+            removeClass($("#answer1"), "fourAvailAnswers");
+            addClass($("#answer1"), "twoAvailAnswers");
+            removeClass($("#answer2"), "fourAvailAnswers");
+            addClass($("#answer2"), "twoAvailAnswers");
+            removeClass($("#answer1"), "borderBottomYellow");
+            removeClass($("#answer2"), "borderBottomYellow");
+            
+            
             $("#quesrow2").hide();
         }
         
@@ -265,44 +334,72 @@ function showNextQuestion() {
     } else {
         var quizRes = quiz.right * 100 / quiz.count;
         //quizRes = 90;
-        $("#quizresult").text(quizRes >= 85 ? "Excellent Job" :
-                             quizRes >= 60 ? "Well Done" : "More Practice Please");
+        $("#quizresult").text(quizRes >= 85 ? "Excellent Job!" :
+                             quizRes >= 60 ? "Well Done!" : "More Practice!");
         var rightWrong = [];
         rightWrong.push(new itemInfo('right', quiz.right));
         rightWrong.push(new itemInfo('wrong', quiz.count-quiz.right));
         addChart('chartrightwrong', 'doughnut', 'Right/Wrong', rightWrong);
         
         addChart('charttimeused', 'line', 'Time Used', quiz.answerTime);
-        showResult(true);
+        showResult(true, true);
         $(".resultChart").show();
-        $("#chartrightwrong div canvas").style ="width: 120px; height: 300px; position: absolute";
-        $("#chartrightwrong div canvas").width = 120;
-        $("#chartrightwrong div canvas").height = 300;
-        $("#charttimeused div canvas").style ="width: 180px; height: 300px; position: absolute";
-        $("#charttimeused div canvas").width = 180;
-        $("#charttimeused div canvas").height = 300;
+        $(".resultChart").resize();
     }
 }
 
+function removeClass($el, cls) {
+    if($el.hasClass(cls)) 
+        $el.removeClass(cls);
+}
+
+function addClass($el, cls) {
+    if(!($el.hasClass(cls)))
+        $el.addClass(cls);
+}
+
 function addChart(idContainer, chartType, chartTitle, dataToDisplay) {
+    $document = $(document);
+    var chartWidth, chartHeight;
+    var dwidth = $document.innerWidth();
+    var dheight = $document.innerHeight();
+    if(dwidth >= 768) {
+        chartWidth = 500;
+        chartHeight = 360;
+    } else {
+        chartWidth = dwidth < 530 ? dwidth - 30 : 500;
+        chartHeight = dheight < 500 ? dheight - 140 : 360;
+    }
+    //console.log("dwidth: "+dwidth+", dheight: "+dheight);
+    //console.log("chartWidth: "+chartWidth+", chartHeight: "+chartHeight);
+    
     var chart = new CanvasJS.Chart(idContainer, {
         theme: "theme2",//theme1
         backgroundColor: "transparent", //custom css, looking on the web docs for it
+        height: chartHeight,
+        width: chartWidth,
         title:{
             text: chartTitle,
-            fontColor: "rgb(255,255,255)"
+            fontColor: "rgb(225,225,225)"
         },
         animationEnabled: false,   // change to true
         axisX: {
-            labelFontColor: "rgb(255,255,255)"
+            labelFontColor: "rgb(225,225,225)"
         },
         axisY: {
-            labelFontColor: "rgb(255,255,255)"
+            labelFontColor: "rgb(225,225,225)"
+        },
+        legend:{
+            verticalAlign: "bottom",
+            horizontalAlign: "center"
         },
         data: [              
         {
             // Change type to "bar", "area", "spline", "pie",etc.
             type: chartType,
+            indexLabelFontColor: "rgb(225,225,225)",
+            indexLabelLineColor: "rgb(225,225,225)",
+            showLegend: true,
             dataPoints: [
             ]
 
@@ -324,14 +421,14 @@ function getCorrectIndex(answerCount) {
     return Math.floor(Math.random() * answerCount);
 }
 
-function clearRadioState() {
+//function clearRadioState() {
 //    $("input[type='radio']").each(function() {
 //        console.log(this.id);
 //        console.log(this.checked);
 //        if(this.checked) this.checked = false;
 //        //removeAttr("checked");
 //    });
-}
+//}
     
 function nextQuestion() {
     if(curQuestion.startTime != null) {
@@ -342,4 +439,45 @@ function nextQuestion() {
     quiz.done++;
     $("#didit").html(quiz.right + " / " + quiz.done);
     showNextQuestion();
+}
+
+function loadCategoriesAndSettings(data) {
+    data.trivia_categories.forEach(function(category, index) {
+        //console.log(category.name);
+        $option = $("<option>");
+        $option.text(category.name);
+        $option.attr("value", category.id);
+        $("#quizcategory").append($option);
+    });
+    //$("#quizresult").text("Ready, give it a go...");
+    loadQuizSettings();
+}
+
+function loadLocalQuizInfo() {
+    $.getJSON("data\categories.json")
+    .done(function(data) {
+        loadCategoriesAndSettings(data);
+    })
+    .fail(function() {
+        console.error("file 'data\categories.json' doesn't exist.");
+    });
+}
+
+function getQuestionCategory() {
+    if(quiz.category) 
+        return quiz.category;
+    else {
+        var max = $("#quizcategory").options.length;
+        var index = Math.floor(Math.random()) * (max - 1) + 1;
+        return $("#quizcategory").options[index].value;
+    }
+}
+
+function getLocalQuestions() {
+    var i=0;
+    do{
+        var category = getQuestionCategory();
+        var type = getQuestionType();
+        var difficulty = getQuestionDifficulty();
+    } while(++i < quiz.count);
 }
